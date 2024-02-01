d9ff16d9de492a7225729cec13acafe40bce9437
bowski43 <43mngt@gmail.com>
2024-02-01 10:34:51 +0300

    initial commit

---
+++ Store/Models/Device.swift
@@ -0,0 +1,8 @@
+//
+//  Device.swift
+//  Store
+//
+//  Created by Serge Bowski on 2/1/24.
+//
+
+import Foundation
---
+++ Store/Services/NetworkManager.swift
@@ -0,0 +1,8 @@
+//
+//  NetworkManager.swift
+//  Store
+//
+//  Created by Serge Bowski on 2/1/24.
+//
+
+import Foundation
